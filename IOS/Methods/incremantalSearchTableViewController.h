5084ba2d4600e6a70e97122bc75942a6165052a4
Daniel Klinkert <danielklinkert@MacBook-Pro-de-Daniel.local>
2014-09-12 16:57:33 -0500

    Se empieza a realizar el primer método

---
+++ IOS/Methods/incremantalSearchTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  incremantalSearchTableViewController.h
+//  Methods
+//
+//  Created by Daniel Klinkert on 9/11/14.
+//  Copyright (c) 2014 _danielKlinkert_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface incremantalSearchTableViewController : UITableViewController
+
+@end
