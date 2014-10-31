c57561738f221e06f978b1f0b4f6e2f02eca5fb2
Andres <andes-20@hotmail.com>
2014-10-31 10:07:16 -0500

    implementacion de las pantallas de sistemas de ecuaciones

---
+++ Android/src/com/example/metodosanalisis/Eliminacion_Gauss.java
@@ -0,0 +1,19 @@
+package com.example.metodosanalisis;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Eliminacion_Gauss extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_secante);
+
+	}
+
+}
---
+++ Android/src/com/example/metodosanalisis/Pivoteo_Escalonado.java
@@ -0,0 +1,19 @@
+package com.example.metodosanalisis;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Pivoteo_Escalonado extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_secante);
+
+	}
+
+}
