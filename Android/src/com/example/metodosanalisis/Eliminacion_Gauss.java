09f1f350559a20f1484e1cc12a6e5d8eebc7fe55
Daniel Klinkert <danielklinkert@Daniels-MacBook-Pro.local>
2014-10-31 14:09:43 -0500

    Se implemento todas las eliminaciones y factorizaciones queda pendiente la interfaz

--- Android/src/com/example/metodosanalisis/Eliminacion_Gauss.java
+++ Android/src/com/example/metodosanalisis/Eliminacion_Gauss.java
@@ -1,26 +1,369 @@
 package com.example.metodosanalisis;
 
+import java.util.Arrays;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class Eliminacion_Gauss extends Activity {
+	
+	CheckBox parcial; 
+	CheckBox total;
+	CheckBox sinPivoteo;
+	CheckBox escalonado;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_eliminacion__gauss);
-
+		parcial = (CheckBox) findViewById (R.id.PivoteoParcial);
+		total 	= (CheckBox) findViewById(R.id.PivoteoTotal);
+		sinPivoteo = (CheckBox)findViewById(R.id.SinPivoteo);
+		escalonado = (CheckBox)findViewById(R.id.PivoteoEscalonado);
+		
 	}
 	
 	public void calcular(View v){
 		
 		Log.i("mierda", "mierda");
+		double[][] matrix = {{-7,2,-3,4,-12},{5,-1,14,-1,13},{1,9,-7,13,31},{-12,13,-8,-4,-32}};
 		
+		
+		//this.eliminacionGaussPivoteoParcial(4, matrix);
+		
+		if(parcial.isChecked() && !total.isChecked() && !sinPivoteo.isChecked() && !escalonado.isChecked()){
+			this.eliminacionGaussPivoteoParcial(4, matrix);
+		}else if(sinPivoteo.isChecked() && !parcial.isChecked()&& !total.isChecked() && !escalonado.isChecked()){
+			this.eliminacionGaussianaSimple(4, matrix);
+			
+		}else if(total.isChecked() && !parcial.isChecked() && !sinPivoteo.isChecked() && !escalonado.isChecked()){
+			this.eliminacionGaussPivoteoTotal(4, matrix);
+		}else if(escalonado.isChecked() && !parcial.isChecked()&& !total.isChecked() && !sinPivoteo.isChecked() ){
+			this.eliminacionGaussPivoteoEscalonado(4, matrix);
+		}else{
+			
+			Log.i("perra","perra");
+		}
 	}
 	
+	// Se implementa pivoteoParcial
+
+	public static void eliminacionGaussPivoteoParcial(int n, double[][] matrix){       
+        System.out.println("Matriz Original");
+        imprimirMatriz(matrix,n);
+      
+        for(int k = 1; k < n; k++){
+            System.out.println("Etapa "+ k);
+            pivoteoParcial(matrix, k, n);
+            System.out.println("Objetivo: Poner ceros debajo del elemento A"+ k +","+k + "= " + matrix[k-1][k-1]);
+            System.out.println("\nMultiplicadores:");
+            for(int i = k+1; i < n+1; i++){
+                double multiplicador = matrix[i-1][k-1]/matrix[k-1][k-1];
+                for(int j = k ; j < n+2; j++){
+                    matrix[i-1][j-1] = matrix[i-1][j-1] - multiplicador*matrix[k-1][j-1];
+                }
+                System.out.println("Multiplicador"+ i+","+k +" : " + multiplicador);
+            }
+            System.out.println(" ");
+            imprimirMatriz(matrix,n);
+        }
+        double x[] = new double[n];
+        System.out.println("Sustitución Regresiva");
+        for(int i = n; i>0;i--){
+                double sumatoria = 0;
+                for(int p = i+1; p <= n; p++){
+                        sumatoria = sumatoria + matrix[i-1][p-1]*x[p-1];
+                }
+                x[i-1] = (matrix[i-1][n]-sumatoria)/matrix[i-1][i-1];
+                System.out.println( "X"+i+" = "+x[i-1]);
+        }
+    }
+    
+     public static double[][] pivoteoParcial(double[][] A, int k, int n){
+            double elementoMayor = Math.abs(A[k-1][k-1]);
+            int filaMayor = k-1;
+            for(int s = k-1; s < n; s++){
+                double nuevoElemento = Math.abs(A[s][k-1]);
+                if( nuevoElemento > elementoMayor){
+                    elementoMayor =  Math.abs(A[s][k-1]);
+                    filaMayor = s;
+                }
+            }
+            System.out.println("Elemento Mayor: " + elementoMayor + " en la fila "+ (filaMayor+1));
+            if(elementoMayor == 0){
+                System.out.println("El sistema no tiene solución única.");
+                System.exit(0);
+            }else{
+                if(filaMayor != k-1){
+                    System.out.println("Cambio de fila "+ k + " con fila " + (filaMayor+1));
+                    for(int i = 0; i < A[0].length; i++){
+                            double aux = A[k-1][i];
+                            A[k-1][i] = A[filaMayor][i];
+                            A[filaMayor][i] = aux;
+                    }
+                    imprimirMatriz(A, n);
+                }
+            }
+        return A;
+    }
+    public static void imprimirMatriz(double [][] matrix, int n){
+        for(int i=0; i< n;i++){
+            for(int j=0; j <n+1; j++){
+                System.out.print(matrix[i][j]);
+                printSpaces(String.valueOf(matrix[i][j]).length(),30);
+            }
+            System.out.print("\n");
+        }
+        System.out.println("");
+    }
+    
+    public static void printSpaces(int n, int k){
+
+        if(n<k){
+            for(int i = 0; i<k-n;i++){
+                System.out.print(" ");
+            }
+        }
+    }
+
+    // Se implementa pivoteoTotal
+    public static void eliminacionGaussPivoteoTotal(int n, double[][] matrix){       
+        System.out.println("Matriz Original");
+        int[] marcas = crearMarcas(n);
+        imprimirMatriz(matrix,n,marcas);
+        for(int k = 1; k < n; k++){
+            System.out.println("Etapa "+ k);
+            pivoteoTotal(matrix, marcas, k, n);
+            System.out.println("Objetivo: Poner ceros debajo del elemento A"+ k +","+k + "= " + matrix[k-1][k-1]);
+            System.out.println("\nMultiplicadores:");
+            for(int i = k+1; i < n+1; i++){
+                double multiplicador = matrix[i-1][k-1]/matrix[k-1][k-1];
+                for(int j = k ; j < n+2; j++){
+                    matrix[i-1][j-1] = matrix[i-1][j-1] - multiplicador*matrix[k-1][j-1];
+                }
+                System.out.println("Multiplicador"+ i+","+k +" : " + multiplicador);
+            }
+            System.out.println(" ");
+            imprimirMatriz(matrix,n,marcas);
+        }
+        double x[] = new double[n];
+        System.out.println("Sustitución Regresiva");
+        for(int i = n; i>0;i--){
+                double sumatoria = 0;
+                for(int p = i+1; p <= n; p++){
+                        sumatoria = sumatoria + matrix[i-1][p-1]*x[p-1];
+                }
+                x[i-1] = (matrix[i-1][n]-sumatoria)/matrix[i-1][i-1];
+                System.out.println( "X"+marcas[i-1]+" = "+x[i-1]);
+        }
+    }
+    
+    public static int[] crearMarcas(int n){
+        int[] marcas = new int[n];
+        for(int i = 0; i < n; i++){
+            marcas[i] = i+1;
+        }
+        return marcas;
+    }
+    
+    public static double[][] pivoteoTotal(double[][] matrix, int[] marcas, int k, int n){
+        double mayor = 0;
+        int filaMayor = k-1;
+        int columnaMayor = k-1;
+        for(int r = k-1; r < n; r++){
+            for(int s = k-1; s < n; s++){
+                if(Math.abs(matrix[r][s]) > mayor){
+                    mayor = Math.abs(matrix[r][s]);
+                    filaMayor = r;
+                    columnaMayor = s;
+                }
+            }
+        }
+        System.out.println("Elemento Mayor: " + mayor + " en la fila "+ (filaMayor+1) + " y la columna "+ (columnaMayor+1));
+        if(mayor == 0){
+            System.out.println("El sistema no tiene solución única.");
+            System.exit(0);
+        }else{
+            if(filaMayor != k-1){
+                System.out.println("Cambio de fila "+ k + " con fila " + (filaMayor+1));
+                for(int i = 0; i < matrix[0].length; i++){
+                    double aux = matrix[k-1][i];
+                    matrix[k-1][i] = matrix[filaMayor][i];
+                    matrix[filaMayor][i] = aux;
+                }
+                imprimirMatriz(matrix, n,marcas);
+            }
+            if(columnaMayor != k-1){
+                System.out.println("Cambio de columna "+ k + " con la columna " + (columnaMayor+1));
+                for(int i = 0; i < n; i++){
+                    double aux = matrix[i][k-1];
+                    matrix[i][k-1] = matrix[i][columnaMayor];
+                    matrix[i][columnaMayor] = aux;
+                }
+                int aux2 = marcas[columnaMayor];
+                marcas[columnaMayor] = marcas[k-1];
+                marcas[k-1] = aux2;
+                imprimirMatriz(matrix, n,marcas);
+            }
+        }
+        return matrix;
+    }
+    public static void imprimirMatriz(double [][] matrix, int n, int [] marcas){
+        for(int i = 0;i<n;i++){
+            System.out.print("X"+marcas[i]);
+            printSpaces(String.valueOf(marcas[i]).length(), 30);
+        }
+        System.out.print("\n");
+        for(int i=0; i< n;i++){
+            for(int j=0; j <n+1; j++){
+                System.out.print(matrix[i][j]);
+                printSpaces(String.valueOf(matrix[i][j]).length(),30);
+            }
+            System.out.print("\n");
+        }
+        System.out.println("");
+    }  
+
+    
+    //Se implementa sinPivoteo
+    
+    public static void eliminacionGaussianaSimple(int n, double[][] matrix){
+        System.out.println("Matriz Original");
+        imprimirMatriz(matrix,n);
+        
+        for(int k = 1; k < n; k++){
+            System.out.println("Etapa "+ k);
+            System.out.println("Objetivo: Poner ceros debajo del elemento A"+ k +","+k + "= " + matrix[k-1][k-1]);
+             System.out.println("\nMultiplicadores:");
+            for(int i = k+1; i < n+1; i++){
+                double multiplicador = matrix[i-1][k-1]/matrix[k-1][k-1];
+                for(int j = k ; j < n+2; j++){
+                    matrix[i-1][j-1] = matrix[i-1][j-1] - multiplicador*matrix[k-1][j-1];
+                }
+                
+               
+                System.out.println("Multiplicador"+ i+","+k +" : " + multiplicador);
+            }
+            System.out.println(" ");
+            imprimirMatriz(matrix,n);
+        }
+        double x[] = new double[n];
+        System.out.println("Sustitución Regresiva");
+        for(int i = n; i>0;i--){
+                double sumatoria = 0;
+                for(int p = i+1; p <= n; p++){
+                        sumatoria = sumatoria + matrix[i-1][p-1]*x[p-1];
+                }
+                x[i-1] = (matrix[i-1][n]-sumatoria)/matrix[i-1][i-1];
+                System.out.println( "X"+i+" = "+x[i-1]);
+        }
+    }
+
+    
+    //Se implementa pivoteoEscalonado
+    public static double[] busquedaDelMayorDeCadaFila(int n, double[][] matrix){
+        double[] s= new double[n];
+        for(int i=1; i<n;i++){
+            for(int j=1;j<n;j++){
+                if(Math.abs(matrix[i-1][j-1])>s[i-1]){
+                    s[i-1] = Math.abs(matrix[i-1][j-1]);
+                }
+            }
+        }
+        return s;
+    }
+    
+    public static void eliminacionGaussPivoteoEscalonado(int n, double[][] matrix){       
+        
+        System.out.println("Matriz Original");
+        imprimirMatriz(matrix,n);
+        double[] s = busquedaDelMayorDeCadaFila(n, matrix);
+        System.out.println("Vector de mayores de cada fila");
+        System.out.println(Arrays.toString(s));
+        System.out.println("");
+        for(int k = 1; k < n; k++){
+            System.out.println("Etapa "+ k);
+            
+            pivoteoEscalonado(matrix, k, n, s);
+            
+            System.out.println("");
+            
+            System.out.println("Objetivo: Poner ceros debajo del elemento A"+ k +","+k + "= " + matrix[k-1][k-1]);
+            
+            System.out.println("\nMultiplicadores:");
+            for(int i = k+1; i < n+1; i++){
+                double multiplicador = matrix[i-1][k-1]/matrix[k-1][k-1];
+                for(int j = k ; j < n+2; j++){
+                    matrix[i-1][j-1] = matrix[i-1][j-1] - multiplicador*matrix[k-1][j-1];
+                }
+                System.out.println("Multiplicador"+ i+","+k +" : " + multiplicador);
+            }
+            System.out.println(" ");
+            imprimirMatriz(matrix,n);
+        }
+        double x[] = new double[n];
+        System.out.println("Sustitución Regresiva");
+        for(int i = n; i>0;i--){
+                double sumatoria = 0;
+                for(int p = i+1; p <= n; p++){
+                        sumatoria = sumatoria + matrix[i-1][p-1]*x[p-1];
+                }
+                x[i-1] = (matrix[i-1][n]-sumatoria)/matrix[i-1][i-1];
+                System.out.println( "X"+i+" = "+x[i-1]);
+        }
+    }
+
+
+    public static void pivoteoEscalonado(double[][] matrix, int k, int n,double[] s) {
+       
+        double mayor = 0;
+        int filamayor = k-1;
+        
+        double[] cocientes = new double[n];
+        
+        for(int i = k; i<n+1;i++){
+            cocientes[i-1] = Math.abs(matrix[i-1][k-1])/s[i-1];
+        }
+        System.out.println("");
+        System.out.println("Vector de cocientes");
+        System.out.println(Arrays.toString(cocientes));
+        
+        for(int i = k-1; i<n;i++){
+            if(cocientes[i]>mayor){
+                mayor = cocientes[i];
+                filamayor = i;
+            }
+        }
+        System.out.println("El cociente mayor es: "+mayor+" de la fila "+(filamayor+1));
+        if(mayor==0){
+                System.out.println("El sistema no tiene solución única.");
+                System.exit(0);
+        }else{
+            if(filamayor!=k-1){
+                System.out.println("");
+                System.out.println("Cambio de fila "+ k + " con fila " + (filamayor+1));
+                for(int i = 0; i < matrix[0].length; i++){
+                    double aux = matrix[k-1][i];
+                    matrix[k-1][i] = matrix[filamayor][i];
+                    matrix[filamayor][i] = aux;
+                }
+                double aux2 = s[k-1];
+                s[k-1] = s[filamayor];
+                s[filamayor] = aux2;
+                
+                System.out.println("Matriz A");
+                imprimirMatriz(matrix, n);
+                System.out.println("Vector de mayores de cada fila");
+                System.out.println(Arrays.toString(s));
+            }
+        }
+    }
+    
 }
--- Android/src/com/example/metodosanalisis/Factorizacion.java
+++ Android/src/com/example/metodosanalisis/Factorizacion.java
@@ -1,19 +1,311 @@
 package com.example.metodosanalisis;
 
+import java.util.Arrays;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class Factorizacion extends Activity {
-	
+	CheckBox crout ;
+	CheckBox doolittle;
+	CheckBox cholesky;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_secante);
-
+		setContentView(R.layout.activity_factorizacion);
+		crout = (CheckBox)findViewById(R.id.Crout);
+		doolittle = (CheckBox)findViewById(R.id.Doolittle);
+		cholesky = (CheckBox)findViewById(R.id.Cholesky);
 	}
+	
+	public void calcular(View v){
+		// cambiar matriz
+		double[][] matrix = {{-7,2,-3,4,-12},{5,-1,14,-1,13},{1,9,-7,13,31},{-12,13,-8,-4,-32}};
+	       double[] b = {-20,69,96,-32};
+	       double[] x0 = {0,0};
+		
+		if(crout.isChecked() && !doolittle.isChecked() && !cholesky.isChecked()){
+			this.factorizacionDirectaCrout(matrix, b, 4);
+		}else if(doolittle.isChecked() && !crout.isChecked()&& !cholesky.isChecked()){
+			this.factorizacionDirectaDoolittle(matrix, b, 4);
+			
+		}else if(cholesky.isChecked() && !doolittle.isChecked() && !crout.isChecked()){
+			this.factorizacionDirectaCholesky(matrix, b, 4);
+		}else{
+			
+			Log.i("perra","perra");
+		}
+		
+	}
+	
+	// Se implementa Crout
+	
+	public static void factorizacionDirectaCrout(double A[][], double[] b, int n){
+        double [][] L = new double[n][n];
+        double [][] U = new double[n][n];
+       
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i<j){
+                   U[i][j] = Double.POSITIVE_INFINITY;
+                   L[i][j] = 0;
+                }else if(i>j){
+                    L[i][j] = Double.POSITIVE_INFINITY;
+                    U[i][j] = 0;
+                }else if(i==j){
+                    U[i][j] = 1;
+                    L[i][j] = Double.POSITIVE_INFINITY;
+                }
+            }
+        }
+                
+        System.out.println("Etapa 0");
+        System.out.println("Matriz A");
+        imprimirMatriz(A, n);
+        System.out.println("Matriz L");
+        imprimirMatriz(L, n);
+        System.out.println("Matriz U");
+        imprimirMatriz(U, n);
+        
+        for(int k = 1; k < n+1; k++){
+            System.out.println("Etapa "+k);
+            System.out.println("Encontrar la columna "+k+" de L y la fila "+ k+" de U.");
+            System.out.println("Matriz A");
+            imprimirMatriz(A, n);
+            double suma = 0;
+            for(int p = 0; p < k-1; p++){
+                suma += L[k-1][p] * U[p][k-1];
+            }
+            L[k-1][k-1] = (A[k-1][k-1] - suma)/U[k-1][k-1];
+            for(int i = k+1; i < n+1; i++){
+                suma = 0;
+                for(int p = 0; p < k-1; p++){
+                    suma += L[i-1][p] * U[p][k-1];
+                }
+                L[i-1][k-1] = (A[i-1][k-1] - suma)/U[k-1][k-1];
+            }
+            
+            System.out.println("Matriz L");
+            imprimirMatriz(L, n);
+            
+            for(int j = k+1; j < n+1; j++){
+                suma = 0;
+                for(int p = 0; p < k-1; p++){
+                    suma += L[k-1][p] * U[p][j-1];
+                }
+                U[k-1][j-1] = (A[k-1][j-1] - suma)/L[k-1][k-1];
+            }
+            System.out.println("Matriz U");
+            imprimirMatriz(U, n);
+        }
+        System.out.println("\nSustitución Progresiva Lz=b");
+        double[] z = sustitucionProgresiva(L, b);
+        System.out.println("Z:"+Arrays.toString(z));
+        System.out.println("\nSustitución Regresiva Ux=z");
+        double[] x = sustitucionRegresiva(U, z);
+        for(int i = 0; i < x.length; i++){
+            System.out.println("X" + (i+1) + " = " + x[i]);
+        }
+    }
+    
+    public static double[] sustitucionProgresiva(double[][] L, double[] b){
+        int n = L.length;
+        double x[] = new double[n];
+        for(int i = 1; i < n+1; i++){
+            double suma = 0;
+            for(int p = i-1; p > 0; p--){
+                double a = L[i-2][p];
+                double c = x[p-1];
+                suma += (L[i-1][p-1] * x[p-1]);
+            }
+            x[i-1] = (b[i-1] - suma) / L[i-1][i-1];
+        }
+        return x;
+    }
 
+    public static double[] sustitucionRegresiva(double[][] U, double[] z){
+        int n = U.length;
+        double[] x = new double[n];
+       
+        for(int i = n-1; i>=0; i--){
+            double suma = 0;
+            for(int j = i+1; j<n; j++){
+                suma += U[i][j] * x[j];
+            }
+            x[i] = (z[i]- suma) /U[i][i];
+        }
+        return x;
+    }
+    
+    public static void imprimirMatriz(double [][] matrix, int n){
+        for(int i=0; i< n;i++){
+            for(int j=0; j <n; j++){
+                System.out.print(matrix[i][j]);
+                printSpaces(String.valueOf(matrix[i][j]).length(),30);
+            }
+            System.out.print("\n");
+        }
+        System.out.println("");
+    }
+    
+    public static void printSpaces(int n, int k){
+        if(n<k){
+            for(int i = 0; i<k-n;i++){
+                System.out.print(" ");
+            }
+        }
+    }
+
+    
+    //Se implementa doolittle
+    public static void factorizacionDirectaDoolittle(double A[][], double[] b, int n){
+        double [][] L = new double[n][n];
+        double [][] U = new double[n][n];
+       
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i<j){
+                   U[i][j] = Double.POSITIVE_INFINITY;
+                   L[i][j] = 0;
+                }else if(i>j){
+                    L[i][j] = Double.POSITIVE_INFINITY;
+                    U[i][j] = 0;
+                }else if(i==j){
+                    L[i][j] = 1;
+                    U[i][j] = Double.POSITIVE_INFINITY;
+                }
+            }
+        }
+        System.out.println("Etapa 0");
+        System.out.println("Matriz A");
+        imprimirMatriz(A, n);
+        System.out.println("Matriz L");
+        imprimirMatriz(L, n);
+        System.out.println("Matriz U");
+        imprimirMatriz(U, n);
+              
+        for(int k = 1; k < n+1; k++){
+            System.out.println("Etapa "+k);
+            System.out.println("Encontrar la columna "+k+" de L y la fila "+ k+" de U.");
+            System.out.println("Matriz A");
+            imprimirMatriz(A, n);
+            double suma = 0;
+            for(int p = 0; p < k-1; p++){
+                suma += L[k-1][p] * U[p][k-1];
+            }
+            U[k-1][k-1] = (A[k-1][k-1] - suma)/L[k-1][k-1];
+            
+            for(int j = k+1; j < n+1; j++){
+                suma = 0;
+                for(int p = 0; p < k-1; p++){
+                    
+                    suma += L[k-1][p] * U[p][j-1];
+                }
+                U[k-1][j-1] = (A[k-1][j-1] - suma)/L[k-1][k-1];
+            }
+            System.out.println("Matriz L");
+            imprimirMatriz(L, n);
+            for(int i = k+1; i < n+1; i++){
+                suma = 0;
+                for(int p = 0; p < k-1; p++){
+                    suma += L[i-1][p] * U[p][k-1];
+                }
+                L[i-1][k-1] = (A[i-1][k-1] - suma)/U[k-1][k-1];
+            } 
+            System.out.println("Matriz U");
+            imprimirMatriz(U, n);
+        }
+        
+        System.out.println("\nSustitución Progresiva Lz=b");
+        double[] z = sustitucionProgresiva(L, b);
+        System.out.println("Z:"+Arrays.toString(z));
+        System.out.println("\nSustitución Regresiva Ux=z");
+        double[] x = sustitucionRegresiva(U, z);
+        for(int i = 0; i < x.length; i++){
+            System.out.println("X" + (i+1) + " = " + x[i]);
+        }
+    }
+    
+    //Se implementa Cholesky
+    public static void factorizacionDirectaCholesky(double A[][], double[] b, int n){
+        double [][] L = new double[n][n];
+        double [][] U = new double[n][n];
+       
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i<j){
+                   U[i][j] = Double.POSITIVE_INFINITY;
+                   L[i][j] = 0;
+                }else if(i>j){
+                    L[i][j] = Double.POSITIVE_INFINITY;
+                    U[i][j] = 0;
+                }else if(i==j){
+                    L[i][j] = Double.POSITIVE_INFINITY;
+                    U[i][j] = Double.POSITIVE_INFINITY;
+                }
+            }
+        }
+        
+        System.out.println("Etapa 0");
+        System.out.println("Matriz A");
+        imprimirMatriz(A, n);
+        System.out.println("Matriz L");
+        imprimirMatriz(L, n);
+        System.out.println("Matriz U");
+        imprimirMatriz(U, n);
+      
+         for(int k = 1; k < n+1; k++){
+            System.out.println("Etapa "+k);
+            System.out.println("Encontrar la columna "+k+" de L y la fila "+ k+" de U.");
+            System.out.println("Matriz A");
+            imprimirMatriz(A, n);
+            double suma = 0;
+            for(int p = 0; p < k-1; p++){
+                suma += L[k-1][p] * U[p][k-1];
+            }
+            U[k-1][k-1] = Math.sqrt(A[k-1][k-1] - suma);
+            L[k-1][k-1] = U[k-1][k-1];
+            
+            for(int j = k+1; j < n+1; j++){
+                suma = 0;
+                for(int p = 0; p < k-1; p++){
+                    
+                    suma += L[j-1][p] * U[p][k-1];
+                }
+                 L[j-1][k-1] = (A[j-1][k-1] - suma) / L[k-1][k-1];
+            }
+            System.out.println("Matriz L");
+            imprimirMatriz(L, n);
+            for(int i = k+1; i < n+1; i++){
+                suma = 0;
+                for(int p = 0; p < k-1; p++){
+                    suma += L[k-1][p] * U[p][i-1];
+                }
+                U[k-1][i-1] = (A[k-1][i-1] - suma)/L[k-1][k-1];
+            } 
+            System.out.println("Matriz U");
+            imprimirMatriz(U, n);
+        }
+        
+        System.out.println("\nSustitución Progresiva Lz=b");
+        double[] z = sustitucionProgresiva(L, b);
+        System.out.println("Z:"+Arrays.toString(z));
+        System.out.println("\nSustitución Regresiva Ux=z");
+        double[] x = sustitucionRegresiva(U, z);
+        for(int i = 0; i < x.length; i++){
+            System.out.println("X" + (i+1) + " = " + x[i]);
+        }
+    }
+    
+    
+    
+
+
+     
 }
