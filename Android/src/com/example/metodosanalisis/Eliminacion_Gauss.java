97703f6983d61136ffd42dbd49dc260e0034289b
Daniel Klinkert <danielklinkert@Daniels-MacBook-Pro.local>
2014-10-31 11:58:12 -0500

    Se agregan cambios en sistema de ecuaciones

--- Android/src/com/example/metodosanalisis/Eliminacion_Gauss.java
+++ Android/src/com/example/metodosanalisis/Eliminacion_Gauss.java
@@ -2,6 +2,7 @@ package com.example.metodosanalisis;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -12,8 +13,14 @@ public class Eliminacion_Gauss extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_secante);
+		setContentView(R.layout.activity_eliminacion__gauss);
 
 	}
-
+	
+	public void calcular(View v){
+		
+		Log.i("mierda", "mierda");
+		
+	}
+	
 }
--- Android/src/com/example/metodosanalisis/SistemasDeEcuaciones.java
+++ Android/src/com/example/metodosanalisis/SistemasDeEcuaciones.java
@@ -2,7 +2,10 @@ package com.example.metodosanalisis;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class SistemasDeEcuaciones extends Activity {
 
@@ -17,6 +20,22 @@ public class SistemasDeEcuaciones extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.sistemas_de_ecuaciones, menu);
 		return true;
+	}
+	
+	public void gauss(View v){
+		Log.i("marica", "marica");
+		Intent gauss = new Intent(SistemasDeEcuaciones.this, Eliminacion_Gauss.class);
+		startActivity(gauss);
+	}
+	
+	public void factorizacion(View v){
+		Intent factorizacion = new Intent(SistemasDeEcuaciones.this,Factorizacion.class );
+		startActivity(factorizacion);
+	}
+	
+	public void iterativos(View v){
+		Intent iterativos = new Intent(SistemasDeEcuaciones.this, Metodos_Iterativos.class);
+		startActivity(iterativos);
 	}
 
 }
